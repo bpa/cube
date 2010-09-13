1a97601ee45c8a6b3e30ab5e0002360f3a8d3a75
Bruce Armstrong <bruce@fortressofgeekdom.org>
2010-09-12 22:48:52 -0600

    Use iframe instead of jQuery.load() Usable for the first time.  Need docs and some polish now.

--- gadget/gadget.js
+++ gadget/gadget.js
@@ -1,26 +1,23 @@
 var index;
 var rows;
-
-  alert('hi');
-  document.body.style.width  = "200px";
-  document.body.style.height = "130px";
-$(document).ready(function() {
-  //document.getElementById('debug').innerHTML = 'Setup';
-  $('#debug').innerHTML = 'Setup';
-  $('#data').load('cfop.html', function(){
-        rows = $('#moves > tbody > tr').length;
-	document.write(rows);
-        show(1)
-    });
-});
+var iframe;
+var moves;
 
 function show(rowid) {
     index = rowid;
-    $('#debug').innerHTML = 'Showing ' + rowid;
-    var row = $('#moves > tbody > tr')[rowid];
+    var row = $('tbody > tr', moves)[rowid];
     var arr = $('td', row);
-    $('#setup').innerHTML = arr[4].innerHTML;
+    document.getElementById('setup').innerHTML = arr[4].innerHTML;
     $('#cube').attr('src', $('img', arr[2]).attr('src'));
+}
+
+function setup() {
+  document.body.style.height = "130px";
+  document.body.style.width  = "200px";
+  iframe = document.getElementById('data').contentWindow.document;
+  moves = iframe.getElementById('moves');
+  rows = $('tbody > tr', moves).length;
+  show(1);
 }
 
 function showNext() {
